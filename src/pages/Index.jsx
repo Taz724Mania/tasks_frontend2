cd5930846c0e6689007e12de8fb864528eb229d3
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-29 13:31:16 -0400

    submit button functional

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,5 @@
 import Task from "../components/Tasks"
 import { Form, useLoaderData } from "react-router-dom"
-import { createAction } from "../actions"
 
 export default function Index(props) {
     const allTasks = useLoaderData()
@@ -21,18 +20,18 @@ export default function Index(props) {
                 <button>Submit</button>
             </Form> */}
 
-            <form class="pure-form pure-form-stacked" action={createAction} method="POST">
-                <label for="title" class="pure-label">Title:</label>
+            <Form className="pure-form pure-form-stacked" action="/create" method="POST">
+                <label htmlFor="title" className="pure-label">Title:</label>
                     <input type="text" name="title" id="title" class="pure-input-1-whole" />
 
-                <label for="details" class="pure-label">Details:</label>
+                <label htmlFor="details" className="pure-label">Details:</label>
                     <input type="text" name="details" id="details" class="pure-input-1-whole" />
 
-                <label for="dueDateTime" class="pure-label">Due Date & Time:</label>
-                    <input type="text" name="dueDateTime" id="dueDateTime" class="pure-input-1-whole" />
+                <label htmlFor="dueDateTime" className="pure-label">Due Date & Time:</label>
+                    <input type="datetime-local" name="dueDateTime" id="dueDateTime" className="pure-input-1-whole" />
 
-                <button type="submit" class="pure-buttonpure-button-primary">Submit</button>
-            </form>
+                <button type="submit" className="pure-buttonpure-button-primary">Submit</button>
+            </Form>
 
             <hr />
             {allTasks.map((task, i) => <Task task={task} key={i}/>)}
